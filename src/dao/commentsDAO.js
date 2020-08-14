759b46d6da2d21495bd92e5a70989c024186cc81
George Crisan <georgerdp@gmail.com>
2020-08-14 15:42:15 +0100

    chore: Ticket 8 delete comments

--- src/dao/commentsDAO.js
+++ src/dao/commentsDAO.js
@@ -109,7 +109,7 @@ export default class CommentsDAO {
       // TODO Ticket: Delete Comments
       // Use the userEmail and commentId to delete the proper comment.
       const deleteResponse = await comments.deleteOne({
-        _id: ObjectId(commentId),
+        _id: ObjectId(commentId), email: userEmail
       })
 
       return deleteResponse
